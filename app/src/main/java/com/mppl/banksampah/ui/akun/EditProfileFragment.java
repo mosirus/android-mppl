92099d7dd0e75d6d16f3b9d7f92be2fbb238ca72
febianbatubara <febianbatubaraa@gmail.com>
2020-03-11 10:55:14 +0700

    Memperbaiki register, mengambail data user dari database

---
+++ app/src/main/java/com/mppl/banksampah/ui/akun/EditProfileFragment.java
@@ -0,0 +1,32 @@
+package com.mppl.banksampah.ui.akun;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.mppl.banksampah.R;
+
+public class EditProfileFragment extends Fragment {
+
+    private ProfileViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(ProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile_edit, container, false);
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        return root;
+    }
+}
