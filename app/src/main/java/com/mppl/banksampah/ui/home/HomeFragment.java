cd6bc1407dd4758208dfbd13f99c26ab36b116a6
PangondionKNaibaho123 <61128660+PangondionKNaibaho123@users.noreply.github.com>
2020-03-12 22:04:14 +0700

    membuat activity tukar poin (jangan di merge dulu)

--- app/src/main/java/com/mppl/banksampah/ui/home/HomeFragment.java
+++ app/src/main/java/com/mppl/banksampah/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.mppl.banksampah.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +15,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.mppl.banksampah.R;
+import com.mppl.banksampah.TukarPoin;
 
 public class HomeFragment extends Fragment {
 
@@ -23,13 +26,26 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        Button btnTukarPoin = (Button) root.findViewById(R.id.btn_tukar);
+        btnTukarPoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pindahTukarPoin();
+            }
+        });
         return root;
     }
+
+    public void pindahTukarPoin(){
+        Intent tukarpoin_intent = new Intent(getActivity(), TukarPoin.class);
+        startActivity(tukarpoin_intent);
+    }
+
 }
