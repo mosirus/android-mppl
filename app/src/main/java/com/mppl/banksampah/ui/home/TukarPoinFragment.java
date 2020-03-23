3e52147b85d69ea1c626fab866b2fd3fff2cedcf
PangondionKNaibaho123 <dionkurniawan130@gmail.com>
2020-03-23 23:47:58 +0700

    membuat RecyclerView pada list barang Tukar Poin

--- app/src/main/java/com/mppl/banksampah/ui/home/TukarPoinFragment.java
+++ app/src/main/java/com/mppl/banksampah/ui/home/TukarPoinFragment.java
@@ -3,23 +3,36 @@ package com.mppl.banksampah.ui.home;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import com.mppl.banksampah.R;
+
+import org.jetbrains.annotations.NotNull;
 
 
 public class TukarPoinFragment extends Fragment implements View.OnClickListener {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    private Button btnListBarang;
+    private RecyclerView rv_listBarang;
+    private RecyclerView.LayoutManager layoutManager;
+
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View root = inflater.inflate(R.layout.fragment_tukar_poin, container, false);
+
         return root;
     }
 
     @Override
     public void onClick(View v) {
+
 
     }
 }
