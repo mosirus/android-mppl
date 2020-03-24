8e39df9ec0d51921ec93d8896557f06a9768cf11
PangondionKNaibaho123 <dionkurniawan130@gmail.com>
2020-03-24 21:38:52 +0700

    Menambahkan fragment list kupon & status pada Tukar poin

--- app/src/main/java/com/mppl/banksampah/ui/home/TukarPoinFragment.java
+++ app/src/main/java/com/mppl/banksampah/ui/home/TukarPoinFragment.java
@@ -20,19 +20,37 @@ import org.jetbrains.annotations.NotNull;
 
 public class TukarPoinFragment extends Fragment implements View.OnClickListener {
 
-    private Button btnListBarang;
+    private Button btnListKupon;
+    private Button btnstatus;
     private RecyclerView rv_listBarang;
     private RecyclerView.LayoutManager layoutManager;
 
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View root = inflater.inflate(R.layout.fragment_tukar_poin, container, false);
 
+        btnListKupon = root.findViewById(R.id.ftpbtn_listkupon);
+        btnListKupon.setOnClickListener(this);
+
+        btnstatus = root.findViewById(R.id.ftpbtn_status);
+        btnstatus.setOnClickListener(this);
+
         return root;
     }
 
     @Override
     public void onClick(View v) {
-
+        if(v.getId() == R.id.ftpbtn_listkupon){
+            ListKuponFragment fragment = new ListKuponFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment, ListKuponFragment.class.getSimpleName())
+                            .addToBackStack(null).commit();
+        }
+        else if(v.getId() == R.id.ftpbtn_status){
+            StatusTukarPoinFragment fragment = new StatusTukarPoinFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment, StatusTukarPoinFragment.class.getSimpleName())
+                    .addToBackStack(null).commit();
+        }
 
     }
 }
