a2fd3f9fb1089170cfeb6d41894076b9bdd58491
PangondionKNaibaho123 <dionkurniawan130@gmail.com>
2020-03-28 23:20:23 +0700

    Mengubah itemView pada List Kupon Tukar Poin ke dalam adapter recyclerview

--- app/src/main/java/com/mppl/banksampah/ui/home/ListKuponFragment.java
+++ app/src/main/java/com/mppl/banksampah/ui/home/ListKuponFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.mppl.banksampah.R;
 
@@ -18,6 +21,9 @@ import org.jetbrains.annotations.NotNull;
 public class ListKuponFragment extends Fragment implements View.OnClickListener {
     private Button btnListBarang;
     private Button btnStatus;
+    private RecyclerView rv_listKupon;
+
+    private String[] nama_kupon = {"Potongan Uang Kuliah Rp250.000,-","Potongan Uang Kuliah Rp500.000,-"};
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
@@ -30,6 +36,11 @@ public class ListKuponFragment extends Fragment implements View.OnClickListener 
 
         btnStatus = root.findViewById(R.id.ftpbtn_status);
         btnStatus.setOnClickListener(this);
+
+        rv_listKupon = root.findViewById(R.id.rvtp_list_kupon);
+        rv_listKupon.setHasFixedSize(true);
+        rv_listKupon.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv_listKupon.setAdapter(new SimpleRVAdapter(nama_kupon));
 
         return root;
 
@@ -51,4 +62,31 @@ public class ListKuponFragment extends Fragment implements View.OnClickListener 
         }
 
     }
+
+    public class SimpleRVAdapter extends RecyclerView.Adapter<ListViewHolder>{
+        private String[] data;
+        public SimpleRVAdapter(String[] dataArgs){
+            data = dataArgs;
+        }
+        public ListViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_kupon,parent,false);
+            ListViewHolder viewHolder = new ListViewHolder(view);
+            return viewHolder;
+        }
+        public void onBindViewHolder(ListViewHolder holder, int position){
+            holder.textView.setText(data[position]);
+        }
+        public int getItemCount(){
+            return data.length;
+        }
+    }
+
+    public static class ListViewHolder extends RecyclerView.ViewHolder{
+        public TextView textView;
+        public ListViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.tv_list_kupon);
+        }
+    }
+
 }
