f51888f2281c2394a885049f03525dda3b5d9886
mosirus <hamonangansitorus80@gmail.com>
2020-04-06 02:22:36 +0700

    Fungsi Request Jemput Sampah

--- app/src/main/java/com/mppl/banksampah/ui/home/JemputSampahFragment.java
+++ app/src/main/java/com/mppl/banksampah/ui/home/JemputSampahFragment.java
@@ -1,29 +1,40 @@
 package com.mppl.banksampah.ui.home;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mppl.banksampah.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.Objects;
 
 public class JemputSampahFragment extends Fragment implements View.OnClickListener {
 
     private HomeViewModel homeViewModel;
+
+    private DatabaseReference ref;
 
     private DatePickerDialog datePickerDialog;
     private SimpleDateFormat dateFormatter;
@@ -33,6 +44,11 @@ public class JemputSampahFragment extends Fragment implements View.OnClickListen
     private Button btnOk;
     private Button btnBatal;
     private Button btnStatusJemput;
+
+    private Spinner spnrJenisSampah;
+    private Spinner spnrNamaSampah;
+    private EditText edtJumlahSampah;
+    private EditText edtLokasiJemput;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -59,8 +75,54 @@ public class JemputSampahFragment extends Fragment implements View.OnClickListen
         btnStatusJemput = root.findViewById(R.id.btn_status_jemput);
         btnStatusJemput.setOnClickListener(this);
 
+        spnrJenisSampah = root.findViewById(R.id.spinner_jenis_sampah);
+        spnrNamaSampah = root.findViewById(R.id.spinner_detail_sampah);
+        edtJumlahSampah = root.findViewById(R.id.edtJumlahSampah);
+        edtLokasiJemput = root.findViewById(R.id.edt_lokasi_jemput);
+
+        ref = FirebaseDatabase.getInstance().getReference().child("JemputSampah");
+
         return root;
     }
+
+    private void RequestJemput() {
+        String jenisSampah = spnrJenisSampah.getSelectedItem().toString();
+        String DetailSampah = spnrNamaSampah.getSelectedItem().toString();
+        String jumlahSampah = edtJumlahSampah.getText().toString();
+        String tanggal = pickedDate.getText().toString();
+        String alamatJemput = edtLokasiJemput.getText().toString();
+        String currentuserId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+        if (validateForm()) {
+            ref.child("IdUser").setValue(currentuserId);
+            ref.child("JenisSampah").setValue(jenisSampah);
+            ref.child("DetailSampah").setValue(DetailSampah);
+            ref.child("Berat").setValue(jumlahSampah);
+            ref.child("Tanggal").setValue(tanggal);
+            ref.child("AlamatJemput").setValue(alamatJemput);
+            ref.child("Status").setValue("Sedang diproses");
+
+            new AlertDialog.Builder(Objects.requireNonNull(getActivity()))
+                    .setTitle("Request Berhasil")
+                    .setMessage("Request kamu berhasil dibuat, Sampah Anda Akan Di Jemput Sesuai Dengan Lokasi Anda")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .show();
+        }
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(edtJumlahSampah.getText().toString())) {
+            edtJumlahSampah.setError("Harap masukkan jumlah sampah");
+            result = false;
+        }
+        return result;
+    }
+
+
 
     @Override
     public void onClick(View v) {
@@ -82,6 +144,10 @@ public class JemputSampahFragment extends Fragment implements View.OnClickListen
             fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment, StatusJemputFragment.class.getSimpleName())
                     .addToBackStack(null).commit();
 
+        }
+
+        else if (v.getId() == R.id.btn_ok){
+            RequestJemput();
         }
 
     }
