4cc8ac95a23e30ee1dc8788ba9683f703c8f6617
febianbatubara <febianbatubaraa@gmail.com>
2020-04-05 23:59:18 +0700

    Menyelesaikan fungsi request antar sampah (user)

--- app/src/main/java/com/mppl/banksampah/ui/home/AntarSampahFragment.java
+++ app/src/main/java/com/mppl/banksampah/ui/home/AntarSampahFragment.java
@@ -1,29 +1,43 @@
 package com.mppl.banksampah.ui.home;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mppl.banksampah.R;
+import com.mppl.banksampah.StartActivity;
+import com.mppl.banksampah.ui.tentang.TentangFragment;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.Objects;
 
 public class AntarSampahFragment extends Fragment implements View.OnClickListener {
 
     private HomeViewModel homeViewModel;
+
+    private DatabaseReference ref;
 
     private DatePickerDialog datePickerDialog;
     private SimpleDateFormat dateFormatter;
@@ -33,19 +47,16 @@ public class AntarSampahFragment extends Fragment implements View.OnClickListene
     private Button btnOk;
     private Button btnBatal;
     private Button btnStatusAntar;
+    private TextView tvDaftarLokasi;
+    private Spinner spnrJenisSampah;
+    private Spinner spnrSatuan;
+    private EditText edtJumlahSampah;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_form_antar, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//
-//        });
 
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
 
@@ -53,12 +64,59 @@ public class AntarSampahFragment extends Fragment implements View.OnClickListene
         btnBatal = root.findViewById(R.id.btn_batal_antar);
         btnBatal.setOnClickListener(this);
         btnOk = root.findViewById(R.id.btn_ok);
+        btnOk.setOnClickListener(this);
+
+        spnrJenisSampah = root.findViewById(R.id.spinner_jenis_sampah);
+        spnrSatuan = root.findViewById(R.id.spinner_satuan);
+        edtJumlahSampah = root.findViewById(R.id.edtJumlahSampah);
+
         imgBtnDatePicker = root.findViewById(R.id.date_picker_toggle);
         imgBtnDatePicker.setOnClickListener(this);
         btnStatusAntar = root.findViewById(R.id.btn_status_antar);
         btnStatusAntar.setOnClickListener(this);
+        tvDaftarLokasi = root.findViewById(R.id.tv_daftar_lokasi);
+        tvDaftarLokasi.setOnClickListener(this);
+
+        ref = FirebaseDatabase.getInstance().getReference().child("AntarSampah");
 
         return root;
+    }
+
+    private void makeRequest() {
+        String jenisSampah = spnrJenisSampah.getSelectedItem().toString();
+        String satuanSampah = spnrSatuan.getSelectedItem().toString();
+        String jumlahSampah = edtJumlahSampah.getText().toString();
+        String tanggal = pickedDate.getText().toString();
+        String currentuserId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+        if (validateForm()) {
+            ref.child("IdUser").setValue(currentuserId);
+            ref.child("JenisSampah").setValue(jenisSampah);
+            ref.child("Berat").setValue(jumlahSampah);
+            ref.child("Satuan").setValue(satuanSampah);
+            ref.child("Tanggal").setValue(tanggal);
+            ref.child("Status").setValue("Sedang diproses");
+
+            new AlertDialog.Builder(Objects.requireNonNull(getActivity()))
+                    .setTitle("Request Berhasil")
+                    .setMessage("Request kamu berhasil dibuat, silahkan antar sampah ke pos terdekat")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .show();
+        }
+
+
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(edtJumlahSampah.getText().toString())) {
+            edtJumlahSampah.setError("Harap masukkan jumlah sampah");
+            result = false;
+        }
+        return result;
     }
 
     @Override
@@ -78,45 +136,33 @@ public class AntarSampahFragment extends Fragment implements View.OnClickListene
             FragmentManager fragmentManager = getFragmentManager();
             fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment, StatusAntarFragment.class.getSimpleName())
                     .addToBackStack(null).commit();
+
+        } else if (v.getId() == R.id.tv_daftar_lokasi) {
+            TentangFragment fragment = new TentangFragment();
+
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment, TentangFragment.class.getSimpleName())
+                    .addToBackStack(null).commit();
+        } else if (v.getId() == R.id.btn_ok) {
+            makeRequest();
         }
     }
 
     private void showDateDialog() {
-
-        /**
-         * Calendar untuk mendapatkan tanggal sekarang
-         */
         Calendar newCalendar = Calendar.getInstance();
-
-        /**
-         * Initiate DatePicker dialog
-         */
         datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
 
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-                /**
-                 * Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
-                 */
-
-                /**
-                 * Set Calendar untuk menampung tanggal yang dipilih
-                 */
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
 
-                /**
-                 * Update TextView dengan tanggal yang kita pilih
-                 */
-                pickedDate.setText("Tanggal dipilih : " + dateFormatter.format(newDate.getTime()));
+                pickedDate.setText(dateFormatter.format(newDate.getTime()));
             }
 
         }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
-        /**
-         * Tampilkan DatePicker dialog
-         */
         datePickerDialog.show();
     }
 
