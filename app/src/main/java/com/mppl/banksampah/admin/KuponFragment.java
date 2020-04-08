90cb3658b273adaf6ae7df860182b09867da14ba
PangondionKNaibaho123 <dionkurniawan130@gmail.com>
2020-04-08 21:36:58 +0700

    Perbaikan UI Fungsi Reward(Admin)

--- app/src/main/java/com/mppl/banksampah/admin/KuponFragment.java
+++ app/src/main/java/com/mppl/banksampah/admin/KuponFragment.java
@@ -17,6 +17,8 @@ import com.mppl.banksampah.R;
 public class KuponFragment extends Fragment implements OnClickListener{
 
     private Button btnTambahReward;
+    private Button btnPermintaanReward;
+    private Button btnRiwayatReward;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +26,12 @@ public class KuponFragment extends Fragment implements OnClickListener{
 
         btnTambahReward = root.findViewById(R.id.btn_tambah_reward);
         btnTambahReward.setOnClickListener(this);
+
+        btnPermintaanReward = root.findViewById(R.id.btnReward_permintaanReward);
+        btnPermintaanReward.setOnClickListener(this);
+
+        btnRiwayatReward = root.findViewById(R.id.btnReward_riwayatReward);
+        btnRiwayatReward.setOnClickListener(this);
 
         return root;
     }
@@ -36,6 +44,19 @@ public class KuponFragment extends Fragment implements OnClickListener{
             fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, TambahRewardFragment.class.getSimpleName())
                     .addToBackStack(null).commit();
         }
+        if(v.getId() == R.id.btnReward_permintaanReward){
+            PermintaanRewardFragment fragment = new PermintaanRewardFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, PermintaanRewardFragment.class.getSimpleName())
+                    .addToBackStack(null).commit();
+        }
+        if(v.getId() == R.id.btnReward_riwayatReward){
+            RiwayatRewardFragment fragment = new RiwayatRewardFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, RiwayatRewardFragment.class.getSimpleName())
+                    .addToBackStack(null).commit();
+        }
+
     }
 }
 
